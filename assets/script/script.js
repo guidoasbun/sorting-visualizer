d6e0782c1128ca2fb155b0e87fac7919621e2444
guidoasbun <gdrodrigo8@gmail.com>
2020-06-08 22:15:58 -0700

    removed comma

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,27 +1,23 @@
-
+let array = []
 
 //initialize Array
 
-let array = []
 
 setArray = () => {
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 200; i++) {
     array.push(randomIntArray(5, 600));
   }
   return array
 }
-
 randomIntArray = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
 array = setArray()
-
 renderArray = (array) => {
   document.getElementById('display-array').innerHTML = array.map((val, index)=> (
   `<div class="array-bar" key="index" style="height: ${val}px"></div>`
 )).join('')
 }
- renderArray(array)
 
+renderArray(array)
 
